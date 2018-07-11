107e2124a360b7a64ca19afe783ee0f9c417605f
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2018-07-11 11:15:18 +0300

    api country tests

--- src/api/country/helpers.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=helpers.js.map
--- src/url.test.ts
+++
@@ -1,9 +0,0 @@
-import { countryListUrl } from './url';
-
-describe('url.ts', () => {
-    describe('countryListUrl', () => {
-        it('Should works correctly', () => {
-            expect(countryListUrl()).toBe('https://www.transfermarkt.com/site/dropDownLaender')
-        });
-    });
-});
