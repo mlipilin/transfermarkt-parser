a75e41751d76682e888ef0452c53e54208de19c1
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2019-01-27 11:09:08 +0300

    fix tests

--- src/api/competition/__tests__/methods.test.ts
+++ src/api/competition/__tests__/methods.test.ts
@@ -3,68 +3,67 @@ import { list } from '../methods';
 describe('src/api/competition/methods.ts', () => {
     describe('list', () => {
         it('Should return correct result for England (id=189)', async () => {
-            const response = [
-                {
-                    countryId: 189,
-                    id: 'GB1',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/gb1.png',
-                    title: 'Premier League',
-                },
-                {
-                    countryId: 189,
-                    id: 'GB2',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/gb2.png',
-                    title: 'Championship',
-                },
-                {
-                    countryId: 189,
-                    id: 'GB3',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/gb3.png',
-                    title: 'League One',
-                },
-                {
-                    countryId: 189,
-                    id: 'GB4',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/gb4.png',
-                    title: 'League Two',
-                },
-                {
-                    countryId: 189,
-                    id: 'CNAT',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/cnat.png',
-                    title: 'National League',
-                },
-                {
-                    countryId: 189,
-                    id: 'GB21',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/gb21.png',
-                    title: 'Premier League 2',
-                },
-                {
-                    countryId: 189,
-                    id: 'GB18',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/gb18.png',
-                    title: 'U18 Premier League',
-                },
-                {
-                    countryId: 189,
-                    id: 'GBFL',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/gbfl.png',
-                    title: 'EFL Trophy',
-                },
-                {
-                    countryId: 189,
-                    id: 'CGB',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/cgb.png',
-                    title: 'EFL Cup',
-                },
-                {
-                    countryId: 189,
-                    id: 'GBCS',
-                    logoUrl: 'https://tmssl.akamaized.net//images/logo/normal/gbcs.png',
-                    title: 'Community Shield',
-                },
-            ];
+            const response = [{
+                "countryId": 189,
+                "id": "GB1",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/gb1.png",
+                "title": "Premier League"
+            }, {
+                "countryId": 189,
+                "id": "GB2",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/gb2.png",
+                "title": "Championship"
+            }, {
+                "countryId": 189,
+                "id": "GB3",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/gb3.png",
+                "title": "League One"
+            }, {
+                "countryId": 189,
+                "id": "GB4",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/gb4.png",
+                "title": "League Two"
+            }, {
+                "countryId": 189,
+                "id": "CNAT",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/cnat.png",
+                "title": "National League"
+            }, {
+                "countryId": 189,
+                "id": "GB21",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/gb21.png",
+                "title": "Premier League 2"
+            }, {
+                "countryId": 189,
+                "id": "GB18",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/gb18.png",
+                "title": "U18 Premier League"
+            }, {
+                "countryId": 189,
+                "id": "GBFL",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/gbfl.png",
+                "title": "EFL Trophy"
+            }, {
+                "countryId": 189,
+                "id": "FAC",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/fac.png",
+                "title": "FA Cup"
+            }, {
+                "countryId": 189,
+                "id": "CGB",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/cgb.png",
+                "title": "EFL Cup"
+            }, {
+                "countryId": 189,
+                "id": "GBCS",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/gbcs.png",
+                "title": "Community Shield"
+            }, {
+                "countryId": 189,
+                "id": "FAYC",
+                "logoUrl": "https://tmssl.akamaized.net//images/logo/normal/fayc.png",
+                "title": "FA Youth Cup"
+            }];
             await expect(list(189)).resolves.toEqual(response);
         });
         it('Should return correct result for France (id=50)', async () => {
