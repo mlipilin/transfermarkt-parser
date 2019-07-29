d2f70b743c5833faa104d0a57f6bcffe10ca3c9e
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2019-07-29 19:54:46 +0300

    up node version

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     transform: {
-        '^.+\\.ts?$': 'ts-jest',
+        '^.+\\.tsx?$': 'ts-jest',
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
     moduleFileExtensions: ['ts', 'js'],
