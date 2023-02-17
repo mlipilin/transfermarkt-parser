2ed4fe75f69e2835f8efaee7ec093cf221e2b7b6
Mikhail Lipilin <lipilin.m@gmail.com>
2023-02-17 18:35:49 +0300

    handle empty countryId

--- packages/demo/pages/api/competition/list.ts
+++ packages/demo/pages/api/competition/list.ts
@@ -3,12 +3,19 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { competition, Competition } from 'transfermarkt-parser'
 
-type Data = Competition[]
+type Data = Competition[] | { error: string }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const competitions = await competition.list(req.query.countryId)
+  if (!req.query?.countryId) {
+    return res.status(422).json({
+      error: 'countryId is required',
+    })
+  }
+
+  const countryId = Number(req.query?.countryId)
+  const competitions = await competition.list(countryId)
   res.status(200).json(competitions)
 }
